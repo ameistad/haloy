61ff44f825c801fca3641cf4af0a27e30abb61c2
Andreas <ameistad@gmail.com>
2025-07-20 22:33:31 +0200

    wip

--- internal/haloy/status.go
+++ internal/haloy/status.go
@@ -16,11 +16,18 @@ import (
 func StatusAppCmd() *cobra.Command {
 	var configPath string
 	var serverURL string
-	ExecuteCommand := &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "status [config-path]",
-		Short: "Show status for an app",
-		Long:  ``,
-		Args:  cobra.MaximumNArgs(1),
+		Short: "Show status for an application",
+		Long: `Show current status of a deployed application using a haloy configuration file.
+
+The path can be:
+- A directory containing haloy.json, haloy.yaml, haloy.yml, or haloy.toml
+- A full path to a config file with supported extension
+- A relative path to either of the above
+
+If no path is provided, the current directory is used.`,
+		Args: cobra.MaximumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			if len(args) > 0 {
 				configPath = args[0]
@@ -38,6 +45,7 @@ func StatusAppCmd() *cobra.Command {
 				targetServer = serverURL
 			}
 
+			ui.Info("Getting status for application: %s using server %s", appConfig.Name, targetServer)
 			ctx, cancel := context.WithTimeout(context.Background(), deploy.DefaultContextTimeout)
 			defer cancel()
 
@@ -60,21 +68,25 @@ func StatusAppCmd() *cobra.Command {
 				fmt.Sprintf("Running container(s): %s", strings.Join(containerIDs, ", ")),
 			}
 
-			ui.Section(appConfig.Name, formattedOutput)
+			ui.Section(fmt.Sprintf("Status for %s", appConfig.Name), formattedOutput)
 		},
 	}
-	return ExecuteCommand
+	return cmd
 }
 
 func displayState(state string) string {
-	switch state {
+	switch strings.ToLower(state) {
 	case "running":
-		return lipgloss.NewStyle().Foreground(ui.Green).Render(state)
-	case "stopped":
-		return lipgloss.NewStyle().Foreground(ui.Red).Render(state)
+		return lipgloss.NewStyle().Foreground(ui.Green).Render("Running")
+	case "restarting":
+		return lipgloss.NewStyle().Foreground(ui.Amber).Render("Restarting")
 	case "paused":
-		return lipgloss.NewStyle().Foreground(ui.Blue).Render(state)
+		return lipgloss.NewStyle().Foreground(ui.Blue).Render("Paused")
+	case "exited":
+		return lipgloss.NewStyle().Foreground(ui.Red).Render("Exited")
+	case "stopped":
+		return lipgloss.NewStyle().Foreground(ui.Red).Render("Stopped")
 	default:
-		return lipgloss.NewStyle().Foreground(ui.LightGray).Italic(true).Render(state)
+		return lipgloss.NewStyle().Foreground(ui.LightGray).Italic(true).Render(strings.Title(state))
 	}
 }
