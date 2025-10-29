dc35f41640a1e84002cbb3b3eb9b62a4d28f2b75
Andreas <ameistad@gmail.com>
2025-10-29 22:39:25 +0100

    farskenm

--- internal/api/handlers_deploy.go
+++ internal/api/handlers_deploy.go
@@ -45,7 +45,7 @@ func (s *APIServer) handleDeploy() http.HandlerFunc {
 			}
 			defer cli.Close()
 
-			if err := deploy.DeployApp(ctx, cli, req.DeploymentID, req.TargetConfig, req.RawAppConfig, deploymentLogger); err != nil {
+			if err := deploy.DeployApp(ctx, cli, req.DeploymentID, req.TargetConfig, req.RollbackAppConfig, deploymentLogger); err != nil {
 				logging.LogDeploymentFailed(deploymentLogger, req.DeploymentID, req.TargetConfig.Name, "Deployment failed", err)
 				return
 			}
